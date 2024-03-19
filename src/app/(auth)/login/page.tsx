c87266f9b2b960e26b4a60883d8eb771250d4852
Akshat <akshatjangir1710@gmail.com>
2024-03-19 20:33:20 +0530

    test commit

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -29,7 +29,7 @@ const Page: FC = () => {
       <div className=" flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8 ">
         <div className="w-full flex flex-col items-center max-w-md space-y-8">
           <div className="flex flex-col items-center gap-8">
-            <Image src={Logo} alt="logo" className="w-20 h-20" />
+            {/* <Image src={Logo} alt="logo" className="w-20 h-20" /> */}
             <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
               Sign in to your account
             </h2>
