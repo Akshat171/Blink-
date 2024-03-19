f3f984df8703bc2b94934409381beab1b50a5d8e
Akshat jangid <81281246+Akshat171@users.noreply.github.com>
2024-03-19 23:55:19 +0530

    border chages

--- src/components/ChatInput.tsx
+++ src/components/ChatInput.tsx
@@ -32,8 +32,8 @@ const ChatInput: FC<ChatInputProps> = ({ chatPartner, chatId }) => {
   };
 
   return (
-    <div className="border-t border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
-      <div className="relative flex-1 overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-indigo-600">
+    <div className="border-t border-gray-400 px-4 pt-4 mb-2 sm:mb-0">
+      <div className="relative flex-1 overflow-hidden rounded-lg shadow-sm ring-1 ring-inset ring-gray-500 focus-within:ring-2 focus-within:ring-indigo-600">
         <TextareaAutosize
           ref={textareaRef}
           onKeyDown={(e) => {
